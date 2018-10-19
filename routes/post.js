457272b4227279716be9e635d598146f1b9e6842
Kamil Brzozowski <idlesoluszyn@gmail.com>
2018-10-19 17:09:30 +0200

    fixed bug with delete

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const knex = require('./../models/knex.js');
 
-router.delete('/delete/:id', (req, res) => {
+router.get('/delete/:id', (req, res) => {
     knex('posts').where({id: req.params.id}).del()
     .then((res) => console.log(`Sucesfully deleted post on ${new Date()} with id of ${req.params.id}`))
     .then(() => res.redirect('/'));
